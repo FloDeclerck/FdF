5b8c5c252aa1a59397bdd4fa7dd0b1addf120d8b
Florian DECLERCK <fdeclerc@e3r2p5.42.fr>
2017-01-25 14:48:45 +0100

    FdF

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: fdeclerc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 15:44:41 by fdeclerc          #+#    #+#             */
-/*   Updated: 2016/11/09 13:40:06 by fdeclerc         ###   ########.fr       */
+/*   Updated: 2017/01/25 13:56:41 by fdeclerc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 size_t				ft_strlen(const char *str)
 {
-	unsigned int	i;
+	size_t	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i] != '\0')
 		i++;
 	return (i);
